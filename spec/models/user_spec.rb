58025f51ecf446e7a00d5583e2bb3d235523f518
deividarauj0 <deivid-ns@hotmail.com>
2023-06-27 11:27:22 -0300

    Capítulo 03, aula 04

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before { @user = FactoryBot.build(:user) }
+  # before { @user = FactoryBot.build(:user) }
+  
+  # O método abaixo é o mesmo que o de cima, porém mais simplificado.
+  
+  # subject { build(:user) }
 
-  it { expect(@user).to respond_to(:email) }
-  it { expect(@user).to respond_to(:name) }
-  it { expect(@user).to respond_to(:password) }
-  it { expect(@user).to respond_to(:password_confirmation) }
-  it { expect(@user).to be_valid }
+  # it { expect(@user).to respond_to(:email) }
+  # it { expect(@user).to respond_to(:name) }
+  # it { expect(@user).to respond_to(:password) }
+  # it { expect(@user).to respond_to(:password_confirmation) }
+  # it { expect(@user).to be_valid }
+
+  # O método abaixo é o mesmo que o de cima, porém mais simplificado.
+
+  it { is_expected.to respond_to(:email) }
+  it { is_expected.to respond_to(:name) }
+  it { is_expected.to respond_to(:password) }
+  it { is_expected.to respond_to(:password_confirmation) }
+  it { is_expected.to be_valid }
 end
