c3ac353869bb8cdd383afc649c533563fcb1bad5
deividarauj0 <deivid-ns@hotmail.com>
2023-07-10 11:01:21 -0300

    Capítulo 04, aula 03. Implementando gerador de token

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,12 @@ class User < ApplicationRecord
     "#{email} - #{created_at} - Token: #{Devise.friendly_token}"
   end
 
+  def generate_authentication_token!
+    begin
+      self.auth_token = Devise.friendly_token
+    end while User.exists?(auth_token: auth_token)
+  end
+
   ## O método #_02_#
   
   # attr_accessor :name
