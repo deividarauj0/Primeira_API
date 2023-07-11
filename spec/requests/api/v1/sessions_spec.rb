3455b53cc5a3c96183196007a5c595946f0c7fd7
deividarauj0 <deivid-ns@hotmail.com>
2023-07-11 11:34:19 -0300

    Capítulo 04, aula 06

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -23,7 +23,20 @@ RSpec.describe 'Sessions API', type: :request do
             end
 
             it 'returns the json data for the user with auth token' do
+                user.reload
                 expect(json_body[:auth_token]).to eq(user.auth_token)
+            end
+        end
+
+        context 'when the credentials are incorrect' do
+            let(:credentials) { { email: user.email, password: 'invalid_password' } }
+
+            it 'returns status code 401' do
+                expect(response).to have_http_status(401)
+            end
+
+            it 'returns the json data for the errors' do
+                expect(json_body).to have_key(:errors)
             end
         end
     end
